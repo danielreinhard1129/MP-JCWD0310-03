aeedb541e003b57b51dec2b64d145d6a065ad49a
firzadev <firzadev9@gmail.com>
2024-04-24 13:59:43 +0700

    chore: add page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-blue-600 font-bold ">MINPRO</h1>
+      <h1 className="text-blue-600 font-bold ">Hello World!!!</h1>
     </div>
   );
 }
