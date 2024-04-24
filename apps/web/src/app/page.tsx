1d842ae6aa766851240137c02cac0d98efba5f61
sepfianda <157484076+sepfianda@users.noreply.github.com>
2024-04-24 13:56:53 +0700

    feat: fix page (#2)

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div>
-      <h1 className="text-blue-600 font-bold ">MBOTTTTT</h1>
+      <h1 className="text-blue-600 font-bold ">MINPRO</h1>
     </div>
   );
 }
