bd09efc800f96cfdca2caaa17311ee16905886b4
firzadev <firzadev9@gmail.com>
2024-04-30 21:33:51 +0700

    chore: install libs

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-blue-600 font-bold ">MINPRO</h1>
       <h1 className="text-blue-600 font-bold ">Hello World!!!</h1>
+      <h1 className="text-red-500 font-bold">Haloo</h1>
+      <Button>Test</Button>
     </div>
   );
 }
