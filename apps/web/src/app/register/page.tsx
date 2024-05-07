2752829c640991914e0dce2d69f113ffac13a73a
firzadev <159623968+firzadev@users.noreply.github.com>
2024-05-07 10:08:17 +0700

    Register (#9)
    * feat: add register API
    
    * feat: mobile landing page
    
    * feat: add register
    
    * feat: add some

---
+++ apps/web/src/app/register/page.tsx
@@ -0,0 +1,128 @@
+'use client';
+import FormInput from '@/components/FormInput';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { RegisterValidationSchema } from './RegisterValidationScehma';
+import React from 'react';
+import { useFormik } from 'formik';
+import { Button } from '@/components/ui/button';
+import useRegister from '@/hooks/api/auth/useRegister';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const Register = () => {
+  const { register } = useRegister();
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      role: '',
+    },
+    // RegisterValidationSchema,
+    validationSchema: RegisterValidationSchema,
+    onSubmit: (values) => {
+      try {
+        register(values);
+      } catch (error) {
+        console.error('Registration error:', error);
+      }
+    },
+  });
+
+  const handleRoleChange = (value: any) => {
+    formik.setFieldValue('role', value);
+  };
+  return (
+    <div className="grid md:grid-cols-2">
+      <div className="bg-blue-900 md:h-full"></div>
+      <div className="p-3">
+        <Card className="w-[350px] md:w-[600px] mx-auto border border-gray-900">
+          <CardHeader>
+            <h1 className="font-semibold text-xl">Register</h1>
+            <h1 className="font-semibold">Sign up and explore the events!</h1>
+          </CardHeader>
+
+          <CardContent>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="grid grid-cols-1 gap-3 text-black">
+                <FormInput
+                  name="username"
+                  type="text"
+                  label=" Username"
+                  placeholder="Username"
+                  value={formik.values.username}
+                  error={formik.errors.username}
+                  isError={
+                    !!formik.touched.username && !!formik.errors.username
+                  }
+                  handleChange={formik.handleChange}
+                  handleBlur={formik.handleBlur}
+                />
+
+                <FormInput
+                  name="email"
+                  type="email"
+                  label="Email"
+                  placeholder="Email"
+                  value={formik.values.email}
+                  error={formik.errors.email}
+                  isError={!!formik.touched.email && !!formik.errors.email}
+                  handleChange={formik.handleChange}
+                  handleBlur={formik.handleBlur}
+                />
+
+                <FormInput
+                  name="password"
+                  type="password"
+                  label="Password"
+                  placeholder="Password"
+                  value={formik.values.password}
+                  error={formik.errors.password}
+                  isError={
+                    !!formik.touched.password && !!formik.errors.password
+                  }
+                  handleChange={formik.handleChange}
+                  handleBlur={formik.handleBlur}
+                />
+                <div className="md:flex md:gap-2 items-center">
+                  <Select onValueChange={handleRoleChange}>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="What do you want to be?" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Roles</SelectLabel>
+                        <SelectItem value="CUSTOMER">Customer</SelectItem>
+                        <SelectItem value="ORGANIZER">Organizer</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormInput
+                    name="role"
+                    type="text"
+                    placeholder="Refferal.."
+                    value={formik.values.role}
+                    error={formik.errors.role}
+                    isError={!!formik.touched.role && !!formik.errors.role}
+                    handleChange={formik.handleChange}
+                    handleBlur={formik.handleBlur}
+                  />
+                </div>
+                <Button type="submit">Register</Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- apps/web/src/components/Testi.tsx
+++ apps/web/src/components/Testi.tsx
@@ -1,5 +1,14 @@
 import Marquee from "react-fast-marquee";
 import EventCard from "./EventCard";
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import EventCard from './EventCard';
+import TestiCard from './TestiCard';
+import { Card } from './ui/card';
+import Image from 'next/image';
+import { CalendarDays, MapPin } from 'lucide-react';
+import { Button } from './ui/button';
+import { Avatar, AvatarImage } from './ui/avatar';
 
 const Testi = () => {
   return (
@@ -19,11 +28,52 @@ const Testi = () => {
           <EventCard />
         </Marquee>
       </div>
+    <div className="p-4 h-[450px] bg-[#18181A] flex items-center mt-3 w-[390px]">
+      <div className="">
+        <h1 className="text-[#8A8A8A] text-[20px]">Latest events</h1>
+        <p className="text-[#8A8A8A] text-[11px] mb-2">
+          "Explore diverse event categories, catering to every interest and
+          occasion, ensuring something for everyone."
+        </p>
+        <Marquee speed={50} className="flex gap-3 max-w-[350px]">
+          <EventCard />
+        </Marquee>
+      </div>
     </div>
   );
 };
 
 export default Testi;
+
+{
+  /* <Card className="max-w-[500px] h-[330px] mx-auto border border-black ">
+  <Image
+    src="/foodevent.jpg"
+    alt="food"
+    width={330}
+    height={50}
+    className="mx-auto p-3 rounded-lg"
+  />
+  <div className="flex justify-between p-1">
+    <h1 className="font-bold text-[15px]">Jakarta Music Event</h1>
+    <h1 className="font-bold text-[15px]">Music</h1>
+  </div>
+  <div className="flex gap-2 p-3">
+    <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" />
+    </Avatar>
+    <h1>Javanaction event planner</h1>
+  </div>
+  <div className="flex gap-2 p-1">
+    <CalendarDays size={15} />
+    <h3 className="font-bold text-[10px]">04 Sept 2024</h3>
+  </div>
+  <div className="flex gap-2 p-1">
+    <MapPin size={15} />
+    <h3 className="font-bold text-[10px]">Jakarta, Indonesia</h3>
+  </div>
+</Card> */
+}
 
 {
   /* <Card className="max-w-[500px] h-[330px] mx-auto border border-black ">
